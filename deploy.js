c25a9c7d5eb46459a855498a66ab01044061bd23
ChrisMatic008 <c.christmon@myemail.indwes.edu>
2022-07-08 17:55:17 -0400

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.86.218.164',
+      host: '34.233.133.46',
       username: 'ubuntu',
       privateKey: 'cec-key.pem'
     })
